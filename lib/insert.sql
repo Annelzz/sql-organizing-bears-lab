6f54d9a01952a1ab3c6e0db2253c1286489a0216
Annelise <annelzz26@gmail.com>
2020-12-07 18:03:54 -0800

    commit tree 336dde41d2a21b65f1db48d6713926d881a4b6fe

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 10, 'M', 'brown', 'zany', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 10, 'M', 'brown', 'zany', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 10, 'M', 'brown', 'zany', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 10, 'M', 'brown', 'zany', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 10, 'M', 'brown', 'zany', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 10, 'M', 'brown', 'zany', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 10, 'M', 'brown', 'zany', 1);
+INSERT INTO bears VALUES (8, NULL, 10, 'M', 'brown', 'zany', 1);
